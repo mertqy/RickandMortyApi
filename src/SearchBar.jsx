d45fab5f79b80a5706eead64085685debf3d59c0
Mert Aktas <mert_aktas07@outlook.com>
2024-06-30 17:32:17 +0300

    .

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -3,7 +3,7 @@ function SearchBar({onSearch}) {
 
     return (
         <>
-            <div className="control">
+            <div className="control fixed">
                 <input className="input container mb-5" type="text" placeholder="Character Name"
                        onChange={(event) => {
                            onSearch(event.target.value);
